841254cc194427c61b7de5bc8c64cb03b3be8948
paramvir232 <paramvirgrewal232@gmail.com>
2024-08-16 11:58:37 +0530

    Scraping song titles and storing it into a list

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-date = input("Which year you would like to travel to? Type the date in this format YYYY-MM-DD: ")
+# date = input("Which year you would like to travel to? Type the date in this format YYYY-MM-DD: ")
+date = '2020-10-09'
 
-response = requests.get(url='https://www.billboard.com/charts/hot-100',params=date)
+response = requests.get(url='https://www.billboard.com/charts/hot-100', params=date)
 html_file = response.text
 
-print(html_file)
+# print(html_file)
+
+soup = BeautifulSoup(html_file, 'html.parser')
+raw_title_list = soup.select('li #title-of-a-story')
+
+final_title_list = [title.get_text().strip() for title in raw_title_list]
+
+print(final_title_list)
